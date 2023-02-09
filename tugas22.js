b2679197ee05863142246892a88f4543577cc4da
dnriski <nugrohoriski159@gmail.com>
2023-02-09 18:44:32 +0700

    first commit

---
+++ tugas22.js
@@ -0,0 +1,8 @@
+const panggilMap = () => {
+  let data = ["Saya", "ingin", "belajar", "bersama"];
+  data.forEach((element, index) => {
+    console.log("item :", element, "index ke", index);
+  });
+};
+
+panggilMap();
